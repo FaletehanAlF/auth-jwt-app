cdb0f19c948127a6eb32cbc5b67ba55a459465d4
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-25 10:12:29 +0700

    update feature

--- frontend/src/data/trustedLogos.tsx
+++ frontend/src/data/trustedLogos.tsx
@@ -10,7 +10,6 @@ export const TRUSTED_LOGOS: LogoItem[] = [
   { src: "/logos/apple.svg", alt: "Apple Inc", href: "https://apple.com", title: "Apple Inc" },
   { src: "/logos/tesla.svg", alt: "Tesla Inc", href: "https://tesla.com", title: "Tesla Inc" },
   { src: "/logos/ibm.svg", alt: "IBM", href: "https://ibm.com", title: "IBM" },
-  { src: "/logos/gojek.svg", alt: "Gojek", href: "https://gojek.com", title: "Gojek" },
   { src: "/logos/bukalapak.svg", alt: "Bukalapak", href: "https://bukalapak.com", title: "Bukalapak" },
   { src: "/logos/docker.svg", alt: "Docker", href: "https://docker.com", title: "Docker" },
   { src: "/logos/figma.svg", alt: "Figma", href: "https://figma.com", title: "Figma" },
